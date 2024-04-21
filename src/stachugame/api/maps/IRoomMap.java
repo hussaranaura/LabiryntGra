8a53f0f25b3af1c7e515dfe596d5e8344bee73b0
MARIUSZMOCARZ4 <165887486+MARIUSZMOCARZ4@users.noreply.github.com>
2024-04-21 16:12:43 +0200

    Aktualizacja interfejsu IRoomMap zapomni

--- src/stachugame/api/maps/IRoomMap.java
+++ src/stachugame/api/maps/IRoomMap.java
@@ -5,4 +5,9 @@ public interface IRoomMap {
      *  funkcja ma przejść po wszystkich postaciach na mapie i uruchomić funkcję update
      */
 	void udpateObjects();
+	/**
+	 * funkcja zwraca pokoje na mapie 
+	 * @return tablica pokoji o rozmiarach 10 x 10
+	 */
+	IRoom[][] getRooms();
 }
