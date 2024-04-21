75d78a77049db36cd1453512dbf6fb6f4c2664c8
cyckimurzynki <167464144+cyckimurzynki@users.noreply.github.com>
2024-04-21 22:39:09 +0200

    Wprowadzenie poprawek do klasy GameRoom

--- src/stachugame/implementation/map/GameRoom.java
+++ src/stachugame/implementation/map/GameRoom.java
@@ -10,26 +10,26 @@ import stachugame.api.maps.Direction;
 import stachugame.api.maps.IRoom;
 
 public class GameRoom implements IRoom {
-	public GameRoom(boolean northOpen, boolean eastOpen, boolean southOpen, boolean westOpen) {
-		
-	}
 	private HashSet<IEntity> entities;
 	private HashSet<IItem> items;
 	private HashMap<Direction, Boolean> exits;
+	public GameRoom(boolean northOpen, boolean eastOpen, boolean southOpen, boolean westOpen) {
+		this.exits = new HashMap<>();
+	}
 	private boolean wasFound = false;
 	@Override
 	public Set<IEntity> getEntities() {
 		// TODO Auto-generated method stub
-		return null;
+		return entities;
 	}
 	@Override
 	public Set<IItem> getItems() {
 		// TODO Auto-generated method stub
-		return null;
+		return items;
 	}
 	@Override
 	public boolean isExitOpen(Direction dir) {
 		// TODO Auto-generated method stub
-		return false;
+		return this.exits.getOrDefault(dir, false);
 	}
 }
