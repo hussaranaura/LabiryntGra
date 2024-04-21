2250e58d0e3fdd51aa851bd32b11c9a345e3bebd
MARIUSZMOCARZ4 <165887486+MARIUSZMOCARZ4@users.noreply.github.com>
2024-04-21 16:29:20 +0200

    Stworzenie interfejsu IItem

--- src/stachugame/api/items/IItem.java
+++ src/stachugame/api/items/IItem.java
@@ -1,4 +1,16 @@
 package stachugame.api.items;
 
+import stachugame.api.entities.IEntity;
+
 public interface IItem {
+	/**
+	 * funkcja która zwraca nazwę przedmiotu
+	 * @return nazwa przedmiotu
+	 */
+	String getItemName();
+	/**
+	 * funkcja która zawiera logikę użycia przedmiotu
+	 * @param user postac która używa przedmiot
+	 */
+	void useItem(IEntity user);
 }
